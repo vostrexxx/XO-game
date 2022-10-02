05ece44de1622b6f681dbc6a79c792c988800bda
vostrexxx <fukksleep74@gmail.com>
2022-10-02 23:10:24 +0300

    Создал интерфйся для самой игры

--- main.py
+++ main.py
@@ -26,7 +26,19 @@ buttons = [[0, 0, 0],
            [0, 0, 0],
            [0, 0, 0]]
 
-label = Label(text= player + " turn", font=("consolas", 40))
+label = Label(text=player + " turn", font=("consolas", 40))
 label.pack(side="top")
+
+reset_button = Button(text="restart", font=("consolas", 20), command=new_game)
+reset_button.pack(side="top")
+
+frame = Frame(window)
+frame.pack()
+
+for row in range(3):
+    for column in range(3):
+        buttons[row][column] = Button(frame, text="", font=("consolas", 40), width=5, height=2,
+                                      command=lambda row=row, column=column: next_turn(row, column))
+        buttons[row][column].grid(row=row,column=column)
 
 window.mainloop()
