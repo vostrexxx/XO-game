ac836059de8acff98e7c0f29fa6824cd409d7ae0
vostrexxx <fukksleep74@gmail.com>
2022-10-02 23:29:02 +0300

    Прописал условие выигрыша

--- main.py
+++ main.py
@@ -2,12 +2,48 @@ from tkinter import *
 import random
 
 
-def next_turn():
-    pass
+def next_turn(row, column):
+    global player
+    if buttons[row][column]["text"] == "" and check_winner() is False:
+        if player == players[0]:
+            buttons[row][column]["text"] = player
+
+            if check_winner() is False:
+                player = players[1]
+                label.config(text=(players[1] + " turn"))
+            elif check_winner() is True:
+                label.config(text=(players[0] + " wins"))
+            elif check_winner() == "Tie":
+                label.config(text=("Tie!"))
+        else:
+            buttons[row][column]["text"] = player
+
+            if check_winner() is False:
+                player = players[0]
+                label.config(text=(players[0] + " turn"))
+            elif check_winner() is True:
+                label.config(text=(players[1] + " wins"))
+            elif check_winner() == "Tie":
+                label.config(text=("Tie!"))
 
 
 def check_winner():
-    pass
+    for row in range(3):
+        if buttons[row][0]["text"] == buttons[row][1]["text"] == buttons[row][0]["text"] != "" :
+            return True
+
+    for column in range(3):
+        if buttons[0][column]["text"] == buttons[1][column]["text"] == buttons[2][column]["text"] != "":
+            return True
+
+    if buttons[0][0]["text"] == buttons[1][1]["text"] == buttons[2][2]["text"] != "":
+        return True
+    elif buttons[0][2]["text"] == buttons[1][1]["text"] == buttons[2][0]["text"] != "":
+        return True
+    elif empty_spaces() is False:
+        return "Tie"
+    else:
+        return False
 
 
 def empty_spaces():
@@ -39,6 +75,6 @@ for row in range(3):
     for column in range(3):
         buttons[row][column] = Button(frame, text="", font=("consolas", 40), width=5, height=2,
                                       command=lambda row=row, column=column: next_turn(row, column))
-        buttons[row][column].grid(row=row,column=column)
+        buttons[row][column].grid(row=row, column=column)
 
 window.mainloop()
